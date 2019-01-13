6c30ebccd6e0f8154218edffeb38eea72a098d40
Jonathan Abourbih <jon.abourbih@gmail.com>
2019-01-13 13:58:26 +0000

    Add readme and a running example

---
+++ README.md
@@ -0,0 +1,8 @@
+# Game of Life in F#
+Basic implementation of Conway's Game of Life in F#.
+
+# How to run
+`dotnet run`
+
+# Tests
+`dotnet test`
--- gameoflife/Program.fs
+++ gameoflife/Program.fs
@@ -1,8 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open gameoflife
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main _ =
+    printfn "Here's a flipper!"
+
+    let flipper = Life.Board(Set.ofList [Life.Cell (0, 1); Life.Cell (1, 1); Life.Cell (2, 1)])
+
+    Life.print flipper
+
+    Life.print (Life.evolve flipper)
     0 // return an integer exit code
